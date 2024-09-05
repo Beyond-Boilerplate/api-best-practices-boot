56fd296200a2c6c316ac28649d184125b0e03a13
sagarpoudel <poudelsagar530@gmail.com>
2024-09-04 20:35:34 -0400

    Initial project setup

---
+++ src/main/java/com/github/sardul3/io/api_best_practices_boot/ApiBestPracticesBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.sardul3.io.api_best_practices_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBestPracticesBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBestPracticesBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sardul3/io/api_best_practices_boot/ApiBestPracticesBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sardul3.io.api_best_practices_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBestPracticesBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
